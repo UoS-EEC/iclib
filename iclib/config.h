00b6c1af01f4097eb33e2e6735106942d82b81b5
sivertism <sliper.sivert@gmail.com>
2019-03-01 14:55:10 +0000

    config cleanup.

--- iclib/config.h
+++ iclib/config.h
@@ -1,37 +1,22 @@
-/**
- * Run configuration for testing
- */
-
 #ifndef SRC_CONFIG_H_
 #define SRC_CONFIG_H_
 
 /* ------ Suspend/Restore Mode ----------------------------------------------*/
-//#define TRACK_MMDATA
+#define TRACK_MMDATA
 #define TRACK_STACK
 
 /* ------ Section sizes -----------------------------------------------------*/
-#define STACK_SIZE                      0x100
-#define DATA_SIZE                       0x400
-#define BSS_SIZE                        0x400
-#define MMDATA_SIZE                     0x800
+#define STACK_SIZE 0x100
+#define DATA_SIZE 0x400
+#define BSS_SIZE 0x400
+#define MMDATA_SIZE 0x800
 
 /* ------ Memory manager ----------------------------------------------------*/
-#define     PAGE_SIZE               128u
-#define     MAX_DIRTY_PAGES         16
+#define PAGE_SIZE 128u
+#define MAX_DIRTY_PAGES 16
 
 /* ------ Threshold Calculation ---------------------------------------------*/
-#define     V_C                   205 // 0.2 V
-#define     VMAX                  3584 // 3.5V
-
-/* ------ Debug/Experiments -------------------------------------------------*/
-//#define TRACK_MODIFIED
-//#define LOGNBYTES_SAVED
-//#define LOGVDROP
-#define LOG_SIZE                1000
-
-#if defined(TRACK_MODIFIED) && defined(TRACK_MMDATA)
-#error "Turn off TRACK_MMDATA when running TRACK_MODIFIED. (otherwise mmdata \
-snapshot never gets updated)."
-#endif
+#define V_C 205    // 0.2 V
+#define VMAX 3584  // 3.5V
 
 #endif /* SRC_CONFIG_H_ */
