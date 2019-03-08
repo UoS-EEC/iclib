483145a9911d1d7d6210217487bc4719edf45aed
sts1u16 <sts1u16@ecs.soton.ac.uk>
2019-03-08 12:06:44 +0000

    Added and tested blinky example.

---
+++ blink/blink.c
@@ -0,0 +1,18 @@
+#include <msp430fr5994.h>
+
+int main(int argc, char* argv[]) {
+    // Stop the watchdog timer
+    WDTCTL = WDTPW + WDTHOLD;
+
+    // Disable the GPIO power-on default high-impedance mode
+    PM5CTL0 &= ~LOCKLPM5;
+    P1DIR |= BIT0;  // Set the direction of P1.0 to OUTPUT
+
+    // Toggle LEDs forever
+    while (1) {
+        for (volatile unsigned int i = 0; i < 50000; i++)
+            ;           // Delay
+        P1OUT ^= BIT0;  // Toggle
+    }
+}
+
