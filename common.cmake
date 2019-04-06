9108b7257e3fffe6062d6054ac007ac98ecc2f1e
sivertism <sivert.sliper@lyse.net>
2019-04-06 15:48:35 +0100

    Cleanup of build system.

---
+++ common.cmake
@@ -0,0 +1,29 @@
+# paths
+set(ICLIB_ROOT "${CMAKE_SOURCE_DIR}/../iclib")
+
+set(CMAKE_TOOLCHAIN_FILE
+    ${CMAKE_CURRENT_LIST_DIR}/msp430-toolchain.cmake)
+
+# Force compiler detection so we can set up flags
+enable_language(C ASM)
+
+include_directories($ENV{MSP430_INC}/include) # MSP430 headers
+add_compile_options(
+    -std=c99
+    -mmcu=msp430fr5994
+    -msmall
+    -g
+    -fno-common
+    -Wall
+    )
+
+# Linker scripts
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS} -T ${CMAKE_SOURCE_DIR}/../msp430fr5994.ld ")
+
+# Add to search path for linker scripts (xx_symbols.ld, included by main linker script)
+link_directories(
+    ${CMAKE_SOURCE_DIR}/..
+    $ENV{MSP430_INC}/include
+    $ENV{MSP430_GCC}/lib/gcc/msp430-elf/7.3.1/
+    )
--- msp430-toolchain.cmake
+++ msp430-toolchain.cmake
@@ -1,32 +1,18 @@
-# Toolchain cmake file for msp430-gcc toolchain
-# See https://github.com/AlexanderSidorenko/msp-cmake/blob/master/cmake/gcc/msp430.cmake
-
 INCLUDE(CMakeForceCompiler)
 
-# Generic flags
-
-set(MSP430_CFLAGS
-    "-std=c99 -mmcu=msp430 -mcpu=msp430 -msmall -g -Wall -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast -Wno-discarded-qualifiers"
-  CACHE STRING "MSP430 compilation flags")
-set(MSP430_LFLAGS
-  "-mmcu=msp430fr5994 -mcpu=msp430 -msmall"
-  CACHE STRING "MSP430 link flags")
-
-# Linker scripts
-set(LSCRIPTS "-T${MSP_GCC_ROOT}/include/msp430fr5994_symbols.ld -T${CMAKE_SOURCE_DIR}/../msp430fr5994.ld ")
-
 # Find toolchain programs
-find_program(MSP430-GCC msp430-elf-gcc ${MSP_GCC_ROOT})
-find_program(MSP430-GXX msp430-elf-g++ ${MSP_GCC_ROOT})
-find_program(MSP430-OBJCOPY msp430-objcopy ${MSP_GCC_ROOT})
-find_program(MSP430-SIZE msp430-size ${MSP_GCC_ROOT})
-find_program(MSP430-OBJDUMP msp430-objdump ${MSP_GCC_ROOT})
+find_program(MSP430-GCC msp430-elf-gcc $ENV{MSP430_ROOT}/bin)
+find_program(MSP430-GXX msp430-elf-g++ $ENV{MSP430_ROOT}/bin)
+find_program(MSP430-OBJCOPY msp430-elf-objcopy $ENV{MSP430_ROOT}/bin)
+find_program(MSP430-SIZE msp430-elf-size $ENV{MSP430_ROOT}/bin)
+find_program(MSP430-OBJDUMP msp430-elf-objdump $ENV{MSP430_ROOT}/bin)
 find_program(MSPDEBUG mspdebug)
 
 # Define toolchain
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER ${MSP430-GCC})
-set(CMAKE_CXX_COMPIER ${MSP430-GXX})
+set(CMAKE_ASM_COMPILER ${MSP430-GCC} CACHE INTERNAL "")
+set(CMAKE_C_COMPILER ${MSP430-GCC} CACHE INTERNAL "")
+set(CMAKE_CXX_COMPIER ${MSP430-GXX} CACHE INTERNAL "")
 
 # Prevent CMake from testing the compilers
 set(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "")
@@ -37,39 +23,8 @@ if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
 endif(NOT CMAKE_BUILD_TYPE)
 
-function(add_msp_executable EXECUTABLE_NAME)
-  if (NOT ARGN)
-    message(FATAL_ERROR "List of source files for target ${EXECUTABLE_NAME} is empty.")
-  endif()
-  set(ELF_FILE ${EXECUTABLE_NAME}.elf)
-
-  add_executable(${EXECUTABLE_NAME} ${ARGN})
-
-  set_target_properties(${EXECUTABLE_NAME} PROPERTIES
-    OUTPUT_NAME ${ELF_FILE}
-    COMPILE_FLAGS "${MSP430_CFLAGS}"
-    LINK_FLAGS " ${LSCRIPTS} ${MSP430_LFLAGS}")
-
-  #Display size info after compilation
-  add_custom_command(TARGET ${EXECUTABLE_NAME} POST_BUILD COMMAND ${MSP430-SIZE} ${ELF_FILE})
-endfunction(add_msp_executable)
-
-function(add_msp_library LIBRARY_NAME)
-  if (NOT ARGN)
-    message(FATAL_ERROR "List of source files for target ${LIBRARY_NAME} is empty.")
-  endif()
-
-  add_library(${LIBRARY_NAME} ${ARGN})
-
-  set_target_properties(
-      ${LIBRARY_NAME} PROPERTIES
-      OUTPUT_NAME ${LIBRARY_NAME}
-      COMPILE_FLAGS "${MSP430_CFLAGS}"
-      LINK_FLAGS " ${LSCRIPTS} ${MSP430_LFLAGS}")
-endfunction(add_msp_library)
-
-function(msp430_add_executable_upload ${EXECUTABLE})
+function(add_msp_upload EXECUTABLE)
   add_custom_target(upload_${EXECUTABLE}
-    COMMAND ${MSPDEBUG} -q rf2500 "prog ${EXECUTABLE}.elf"
+    COMMAND ${MSPDEBUG} tilib "prog ${EXECUTABLE}.elf"
     DEPENDS ${EXECUTABLE})
-endfunction(msp430_add_executable_upload)
+endfunction(add_msp_upload)
